275cb90f7722d95db8e7eb34e427be4c0c73a9ed
MD.Sameer Sayed <sameer0sayed@gmail.com>
2025-02-19 16:36:30 +0600

    Added a test file for R basics

---
+++ Codes/test_file.R
@@ -0,0 +1,47 @@
+#First Section ####
+#hi this is first scetion 
+## This is subsection of first section ####
+ 9*4
+
+9/5
+
+9/0
+
+ typeof(6)
+
+ typeof(6L)
+
+#whenever a variable is defined in this console it will be shownin the global environments on the right panel
+   a=6
+   #here a is an object
+   
+#Second Section ####
+   #Hi this is second section 
+   ## THis is subsection of Second section ####
+   #Haha welcome back again 
+a=c(4,5,6)
+typeof(a)   
+b=c(4,5,6L)
+typeof(b)
+x<-c(1,2,3)
+num<-x
+rm(x)
+c=c("A","b")
+A<-1:100
+A
+B<-100:1
+B
+
+m1<-matrix(c(1,2,3,4,5,6),nrow=3)
+m1
+m2<-matrix(c(1,2,3,4,5,6),ncol=3)
+m2
+m2 %*% m1
+
+
+x1<-c("Apple","Samsung","Nokia")
+x1
+"Vivo" %in% x1
+"Apple" %in% x1
+"apple" %in% x1
+
